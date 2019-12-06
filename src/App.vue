4fe8ae290d62d5206f07c643fffb539ef530588c
Femi Balogun <phembarl@yahoo.com>
2019-12-06 13:58:31 +0100

    vuex actions

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <AddTodo />
+      <FilterTodos />
+      <Todos />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todos: () => import('@/components/Todos'),
+    AddTodo: () => import ('@/components/AddTodo'),
+    FilterTodos: () => import ('@/components/FilterTodos')
   }
 }
 </script>
 
 <style>
-#app {
+body {
+  /* font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; */
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
 }
 </style>
